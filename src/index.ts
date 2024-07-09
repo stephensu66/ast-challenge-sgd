75b4dffd30ee703991cb38bc677e00b302906092
stephensu66 <stephen.su66@gmail.com>
2024-07-10 09:21:50 +1000

    finish the ts to ast function

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,107 @@
-export default () => {
+import * as t from '@babel/types';
+import generate from '@babel/generator';
 
-};
+export default function generateAST(
+    queryInterface: string,
+    hookName: string,
+    requestType: string,
+    responseType: string,
+    queryServiceMethod: string,
+    keyName: string
+): string  {
+    
+        const interfaceDeclaration = t.exportNamedDeclaration(
+            t.tsInterfaceDeclaration(
+                t.identifier(queryInterface),
+                null,
+                [],
+                t.tsInterfaceBody([
+                    t.tsPropertySignature(
+                        t.identifier('request'),
+                        t.tsTypeAnnotation(t.tsTypeReference(t.identifier(requestType))),
+                        false
+                    ),
+                    t.tsPropertySignature(
+                        t.identifier('extends'),
+                        t.tsTypeAnnotation(
+                            t.tsExpressionWithTypeArguments(
+                                t.identifier('ReactQueryParams'),
+                                [
+                                    t.tsTypeReference(t.identifier(responseType)),
+                                    t.tsTypeReference(t.identifier('TData'))
+                                ]
+                            )
+                        ),
+                        true
+                    )
+                ])
+            ),
+            []
+        );
+    
+        const constDeclaration = t.variableDeclaration('const', [
+            t.variableDeclarator(
+                t.identifier(hookName),
+                t.arrowFunctionExpression(
+                    [
+                        t.tsTypeParameterDeclaration([
+                            t.tsTypeParameter(
+                                null,
+                                t.tsTypeReference(t.identifier(responseType)),
+                                'TData'
+                            )
+                        ]),
+                        t.objectPattern([
+                            t.objectProperty(t.identifier('request'), t.identifier('request'), false, true),
+                            t.objectProperty(t.identifier('options'), t.identifier('options'), false, true)
+                        ])
+                    ],
+                    t.blockStatement([
+                        t.returnStatement(
+                            t.callExpression(
+                                t.identifier('useQuery'),
+                                [
+                                    t.tsTypeParameterInstantiation([
+                                        t.tsTypeReference(t.identifier(responseType)),
+                                        t.tsTypeReference(t.identifier('Error')),
+                                        t.tsTypeReference(t.identifier('TData'))
+                                    ]),
+                                    t.arrayExpression([
+                                        t.stringLiteral(keyName),
+                                        t.identifier('request')
+                                    ]),
+                                    t.arrowFunctionExpression(
+                                        [],
+                                        t.blockStatement([
+                                            t.ifStatement(
+                                                t.unaryExpression('!', t.identifier('queryService')),
+                                                t.throwStatement(
+                                                    t.newExpression(t.identifier('Error'), [
+                                                        t.stringLiteral('Query Service not initialized')
+                                                    ])
+                                                )
+                                            ),
+                                            t.returnStatement(
+                                                t.callExpression(
+                                                    t.memberExpression(
+                                                        t.identifier('queryService'),
+                                                        t.identifier(queryServiceMethod)
+                                                    ),
+                                                    [t.identifier('request')]
+                                                )
+                                            )
+                                        ])
+                                    ),
+                                    t.identifier('options')
+                                ]
+                            )
+                        )
+                    ])
+                )
+            )
+        ]);
+    
+        const program = t.program([interfaceDeclaration, constDeclaration]);
+    
+        return generate(t.file(program)).code;
+    };
